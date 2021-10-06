c85d1bc4cbec8ae44f3447c455c0656f19a076c4
mandigo.skyler <mandigo.skyler@vashonsd.org>
2021-10-06 10:39:25 -0700

    Application

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println(squareOf(7));
+
+    }
+    public static int squareOf(int n){
+        return n*n;
+    }
+}
